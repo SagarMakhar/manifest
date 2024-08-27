38055803882319188ab3a5603ef5161cc77acecd
Celeste Valambhia <celeste.valambhia@softing.com>
2024-08-27 13:01:57 +0200

    Update ci-build.sh
    Reverted Repo Sync

--- .az/ci-build.sh
+++ .az/ci-build.sh
@@ -647,8 +647,6 @@ ${updateall} && {
 
 ${lmmanipulation} && {
 	lm_setup "${lmfile}"
-	info "Syncing the local manifest files"
-        ./softing-build.sh -p "${phonetarget}" -c -- repo sync --no-repo-verify -v -j12 --force-sync --no-clone-bundle
 }
 
 ${build} && {
